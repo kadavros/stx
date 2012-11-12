b3cb0eb879f1118a3f011af515956f1cb32511c9
kadavros <kadavros@gmail.com>
2012-11-12 17:30:32 +0400

    max_alignment

---
+++ stx/max_alignment.hpp
@@ -0,0 +1,38 @@
+#ifndef STX_MAX_ALIGNMENT_HPP
+#define STX_MAX_ALIGNMENT_HPP
+
+//  Duplication of STX_HAS_LONG_LONG: #ifdef LLONG_MAX
+#include <limits.h>
+//  Duplication of STX_HAS_LONG_DOUBLE: #ifdef LDBL_MANT_DIG
+#include <float.h>
+
+namespace stx {
+
+class max_alignment_helper_class {};
+
+//  This is not standard, but should work with all compilers.
+union max_alignment
+{
+    char        char_;
+    short       short_;
+    int         int_;
+    long        long_;
+#   ifdef LLONG_MAX
+    long long   long_long_;
+#   endif
+    float       float_;
+    double      double_;
+#   ifdef LDBL_MANT_DIG
+    long double long_double_;
+#   endif
+    void        *void_ptr_;
+    void        (*void_func_ptr_)();
+#   ifdef __cplusplus
+    void        max_alignment_helper_class::*void_member_ptr_;
+    void        (max_alignment_helper_class::*void_member_func_ptr_)();
+#   endif
+};
+
+} // namespace stx
+
+#endif // STX_MAX_ALIGNMENT_HPP
