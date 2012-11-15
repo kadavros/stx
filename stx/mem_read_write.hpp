735148aa31712ac9599a9faca5fdee402d792b6c
kadavros <kadavros@gmail.com>
2012-11-15 18:42:51 +0400

    comment fix

--- stx/mem_read_write.hpp
+++ stx/mem_read_write.hpp
@@ -7,7 +7,7 @@
         void* p = malloc(2*sizeof(int));
         int x = 123, y = 0;
         char* p2 = (char*) p + 1;
-        // *(int*) p2 = x; // Nndefined behavior - program may crash on some
+        // *(int*) p2 = x; // Undefined behavior - program may crash on some
         // y = *(int*) p2; // platforms due to unaligned memory access.
         mem_write(p2, x); // Ok
         mem_read(p2, y);  // Ok
