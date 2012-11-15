55d8cb600f907a2ed32deb6c374249a8ac15892e
kadavros <kadavros@gmail.com>
2012-11-15 12:13:27 +0400

    some new macro definitions in "config" module + typedefs in "common" module

--- stx/common.hpp
+++ stx/common.hpp
@@ -12,6 +12,19 @@
 #define STX_CSTR_(x) #x
 #define STX_CSTR(x) STX_CSTR_(x)
 
+namespace stx {
 
+typedef signed char        schar;
+typedef unsigned char      uchar;
+typedef unsigned short     ushort;
+typedef unsigned int       uint;
+typedef unsigned long      ulong;
+typedef long double        ldouble;
+#if STX_HAS_LONG_LONG
+typedef long long          llong;
+typedef unsigned long long ullong;
+#endif
+
+} // namespace stx
 
 #endif // STX_COMMON_HPP
--- stx/config.hpp
+++ stx/config.hpp
@@ -24,6 +24,45 @@
 #   define STX_HAS_LONG_LONG 0
 #endif
 
+#if CHAR_MAX < UCHAR_MAX
+#define STX_CHAR_IS_SIGNED   1
+#define STX_CHAR_IS_UNSIGNED 0
+#else
+#define STX_CHAR_IS_SIGNED   0
+#define STX_CHAR_IS_UNSIGNED 1
+#endif
+
+//  Minimum and maximum values for built-in integer types.
+//  We use ((unsigned long) 0) version for unsigned minimum values
+//  instead of version with suffix 0UL, because suffixes are known
+//  to be not absolutely portable across different implementations
+//  at least for some types.
+#define STX_BITS_IN_BYTE CHAR_BIT
+#define STX_CHAR_MIN     CHAR_MIN
+#define STX_CHAR_MAX     CHAR_MAX
+#define STX_SCHAR_MIN    SCHAR_MIN
+#define STX_SCHAR_MAX    SCHAR_MAX
+#define STX_UCHAR_MIN    ((unsigned char) 0)
+#define STX_UCHAR_MAX    UCHAR_MAX
+#define STX_SHORT_MIN    SHRT_MIN
+#define STX_SHORT_MAX    SHRT_MAX
+#define STX_USHORT_MIN   ((unsigned short) 0)
+#define STX_USHORT_MAX   USHRT_MAX
+#define STX_INT_MIN      INT_MIN
+#define STX_INT_MAX      INT_MAX
+#define STX_UINT_MIN     ((unsigned int) 0)
+#define STX_UINT_MAX     UINT_MAX
+#define STX_LONG_MIN     LONG_MIN
+#define STX_LONG_MAX     LONG_MAX
+#define STX_ULONG_MIN    ((unsigned long) 0)
+#define STX_ULONG_MAX    ULONG_MAX
+#if STX_HAS_LONG_LONG
+#define STX_LLONG_MIN    LLONG_MIN
+#define STX_LLONG_MAX    LLONG_MAX
+#define STX_ULLONG_MIN   ((unsigned long long) 0)
+#define STX_ULLONG_MAX   ULLONG_MAX
+#endif
+
 #include <float.h>
 
 #ifdef LDBL_MANT_DIG
@@ -31,5 +70,9 @@
 #else
 #   define STX_HAS_LONG_DOUBLE 0
 #endif
+
+#define STX_MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define STX_MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define STX_EQUAL(x, y) ((x) == (y))
 
 #endif /* STX_CONFIG_HPP */
