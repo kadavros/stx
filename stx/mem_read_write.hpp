3229ec4dfbc1c524cbb531e501ac3a7094972e80
kadavros <kadavros@gmail.com>
2012-11-15 18:42:35 +0400

    mem_read_write: unaligned memory access helper functions

---
+++ stx/mem_read_write.hpp
@@ -0,0 +1,35 @@
+#ifndef STX_MEM_READ_WRITE_HPP
+#define STX_MEM_READ_WRITE_HPP
+
+/*
+    Unaligned memory read/write.
+    Example:
+        void* p = malloc(2*sizeof(int));
+        int x = 123, y = 0;
+        char* p2 = (char*) p + 1;
+        // *(int*) p2 = x; // Nndefined behavior - program may crash on some
+        // y = *(int*) p2; // platforms due to unaligned memory access.
+        mem_write(p2, x); // Ok
+        mem_read(p2, y);  // Ok
+        free(p);
+*/
+
+#include <string.h>
+
+namespace stx {
+
+template <class T>
+inline void mem_read(const void* p, T& t)
+{
+    memcpy(&t, p, sizeof(T));
+}
+
+template <class T>
+inline void mem_write(void* p, const T& t)
+{
+    memcpy(p, &t, sizeof(T));
+}
+
+} // namespace stx
+
+#endif // STX_MEM_READ_WRITE_HPP
