cb42c0dded273b30e50ce06498a6d791adf87d34
kadavros <kadavros@gmail.com>
2012-11-20 18:41:00 +0400

    log additioal macros

--- stx/log.hpp
+++ stx/log.hpp
@@ -1,15 +1,17 @@
 #ifndef STX_LOG_HPP
 #define STX_LOG_HPP
 
-#define STX_LOG_TRACE if (stx::log_level_trace < get_logger().level()); else get_logger().trace()
-#define STX_LOG_DEBUG if (stx::log_level_debug < get_logger().level()); else get_logger().debug()
-#define STX_LOG_INFO  if (stx::log_level_info  < get_logger().level()); else get_logger().info()
-#define STX_LOG_WARN  if (stx::log_level_warn  < get_logger().level()); else get_logger().warn()
-#define STX_LOG_ERROR if (stx::log_level_error < get_logger().level()); else get_logger().error()
-#define STX_LOG_FATAL if (stx::log_level_fatal < get_logger().level()); else get_logger().fatal()
+#define STX_LOG(lvl)   if (lvl < get_logger().level()); else get_logger().log(lvl)
+#define STX_LOG_TRACE  if (stx::log_level_trace < get_logger().level()); else get_logger().trace()
+#define STX_LOG_DEBUG  if (stx::log_level_debug < get_logger().level()); else get_logger().debug()
+#define STX_LOG_INFO   if (stx::log_level_info  < get_logger().level()); else get_logger().info()
+#define STX_LOG_WARN   if (stx::log_level_warn  < get_logger().level()); else get_logger().warn()
+#define STX_LOG_ERROR  if (stx::log_level_error < get_logger().level()); else get_logger().error()
+#define STX_LOG_FATAL  if (stx::log_level_fatal < get_logger().level()); else get_logger().fatal()
 
 //  Warning: some of those short macros conflict with syslog macros.
 #ifdef STX_NO_MACRO_PREFIX
+#   define LOG       STX_LOG
 #   define LOG_TRACE STX_LOG_TRACE
 #   define LOG_DEBUG STX_LOG_DEBUG
 #   define LOG_INFO  STX_LOG_INFO
