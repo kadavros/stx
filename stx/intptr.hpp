ef2043d685ab0f20b530bad0d9c4b4cc70c092a4
kadavros <kadavros@gmail.com>
2012-11-29 19:37:42 +0400

    intptr - enhanced version

--- stx/intptr.hpp
+++ stx/intptr.hpp
@@ -3,17 +3,61 @@
 
 //  Typedefs for intptr_t & uintptr_t.
 
+#include <stddef.h>
 #include <limits.h>
 
 namespace stx {
 
-#ifdef LLONG_MAX
-typedef long long intptr_t;
-typedef unsigned long long uintptr_t;
-#else
-typedef long intptr_t;
-typedef unsigned long uintptr_t;
+template <size_t N>
+struct intptr_t_impl;
+
+template <>
+struct intptr_t_impl<sizeof(char)>
+{
+    typedef signed char signed_type;
+    typedef unsigned char unsigned_type;
+};
+
+#if USHRT_MAX != UCHAR_MAX
+template <>
+struct intptr_t_impl<sizeof(short)>
+{
+    typedef short signed_type;
+    typedef unsigned short unsigned_type;
+};
 #endif
+
+#if UINT_MAX != USHRT_MAX
+template <>
+struct intptr_t_impl<sizeof(int)>
+{
+    typedef int signed_type;
+    typedef unsigned int unsigned_type;
+};
+#endif
+
+#if ULONG_MAX != UINT_MAX
+template <>
+struct intptr_t_impl<sizeof(long)>
+{
+    typedef long signed_type;
+    typedef unsigned long unsigned_type;
+};
+#endif
+
+#ifdef ULLONG_MAX
+#if ULLONG_MAX != ULONG_MAX
+template <>
+struct intptr_t_impl<sizeof(long long)>
+{
+    typedef long long signed_type;
+    typedef unsigned long long unsigned_type;
+};
+#endif
+#endif
+
+typedef intptr_t_impl<sizeof(void*)>::signed_type intptr_t;
+typedef intptr_t_impl<sizeof(void*)>::unsigned_type uintptr_t;
 
 } // namespace stx
 
