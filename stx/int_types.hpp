85fc89bc4eb2df7214f603047ab0d584131e016c
kadavros <kadavros@gmail.com>
2012-12-03 13:45:59 +0400

    int_types

---
+++ stx/int_types.hpp
@@ -0,0 +1,15 @@
+#ifndef STX_INT_TYPES_HPP
+#define STX_INT_TYPES_HPP
+
+#include <stx/int_types/exact_int.hpp>
+
+//  todo: padding of integer types
+//  todo: least_int
+
+namespace stx {
+
+
+
+} // namespace stx
+
+#endif // STX_INT_TYPES_HPP
