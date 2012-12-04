532a8c26a33d4fc3801bc72ebdce392df7245b09
kadavros <kadavros@gmail.com>
2012-12-04 16:42:10 +0400

    is_same

--- stx/type_traits.hpp
+++ stx/type_traits.hpp
@@ -4,6 +4,7 @@
 #include <stx/type_traits/integral_constant.hpp>
 #include <stx/type_traits/bool_types.hpp>
 #include <stx/type_traits/is_integer_typedef.hpp>
+#include <stx/type_traits/is_same.hpp>
 
 namespace stx {
 
