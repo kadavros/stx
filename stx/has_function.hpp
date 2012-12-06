816ef7ff58ddb1a6cf23f69105e8cf7a6079c022
kadavros <kadavros@gmail.com>
2012-12-06 13:44:06 +0400

    has_function added, has_snprintf updated

---
+++ stx/has_function.hpp
@@ -0,0 +1,75 @@
+#ifndef STX_HAS_FUNCTION_HPP
+#define STX_HAS_FUNCTION_HPP
+
+/*
+    STX_DECLARE_HAS_FUNCTION macro declares a function presence checking function
+    named has_F(), where F - name of a function, which presence we want to check.
+    has_F() is a portable runtime check. If there is no function with given
+    name and signature, has_F() returns false, otherwise it returns true.
+    We can't call function of interest directly, because it just won't compile
+    on platform where it's absent, but we can access it via pointer.
+    STX_DECLARE_HAS_FUNCTION declares such pointer returning function with
+    suffix _ptr, added to original function's name.
+    
+    Usage example. Suppose we wand to check wether snprintf is supported.
+    
+    #include <stdio.h> // We must include header where snprintf may be declared.
+    STX_DECLARE_HAS_FUNCTION(snprintf, int, (char*, size_t, const char*, ...))
+    #include <iostream>
+    
+    int main(int argc, char** argv)
+    {
+        char buf[256] = { 0 };
+        int x = 12345;
+        if (has_snprintf()) {
+            std::cout << "snprintf is present" << std::endl;
+        } else {
+            std::cout << "snprintf is absent" << std::endl;
+        }
+        try {
+            snprintf_ptr()(buf, 6, "%d", x);
+            std::cout << buf << std::endl;
+        } catch (std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
+        return 0;
+    }
+    
+    Note: some function may be declared as macros, therefore complete function
+    checking facility might be more complicated. Considering snprintf example,
+    complete function presence checking looks like this:
+    
+    #include <stdio.h> // We must include header where snprintf may be declared.
+    #ifdef snprintf    // Check, whether snprintf is declared as macro.
+    inline bool has_snprintf() { return true; };
+    inline int (*snprintf_ptr())(char*, size_t, const char*, ...) { return snprintf; }
+    #else
+    STX_DECLARE_HAS_FUNCTION(snprintf, int, (char*, size_t, const char*, ...))
+    #endif
+*/
+
+#include <stdexcept>
+
+#define STX_DECLARE_HAS_FUNCTION(function_name, return_type, param_types) \
+    \
+    struct has_##function_name##_t {}; \
+    \
+    inline return_type function_name(has_##function_name##_t) \
+    { \
+        throw std::runtime_error("function " #function_name " is absent"); \
+    } \
+    \
+    inline bool has_##function_name() \
+    { \
+        void (*fp1)() = (void (*)()) \
+            ((return_type (*)(has_##function_name##_t)) function_name); \
+        void (*fp2)() = (void (*)()) \
+            ((return_type (*)param_types) function_name); \
+        return (fp1 != fp2); \
+    } \
+    inline return_type (*function_name##_ptr())param_types \
+    { \
+        return (return_type (*)param_types) function_name; \
+    }
+
+#endif // STX_HAS_FUNCTION_HPP
--- stx/has_snprintf.hpp
+++ stx/has_snprintf.hpp
@@ -1,19 +1,15 @@
 #ifndef STX_HAS_SNPRINTF_HPP
 #define STX_HAS_SNPRINTF_HPP
 
+#include <stx/has_function.hpp>
+
 #include <stdio.h>
 
 #ifdef snprintf
-inline bool has_snprintf() { return true; }
+inline bool has_snprintf() { return true; };
+inline int (*snprintf_ptr())(char*, size_t, const char*, ...) { return snprintf; }
 #else
-inline void snprintf() {}
-inline bool has_snprintf()
-{
-    void (*fp1)() = snprintf;
-    void (*fp2)() = (void (*)()) ((int (*)(char*, size_t, const char*, ...)) snprintf);
-    return (fp1 != fp2);
-}
-#define snprintf ((int (*)(char*, size_t, const char*, ...)) snprintf)
+STX_DECLARE_HAS_FUNCTION(snprintf, int, (char*, size_t, const char*, ...))
 #endif
 
 #endif // STX_HAS_SNPRINTF_HPP
