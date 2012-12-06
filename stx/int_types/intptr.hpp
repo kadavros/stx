e9fd241621970926ef58dab9140873387e01c8d8
kadavros <kadavros@gmail.com>
2012-12-06 16:03:32 +0400

    intptr update

--- stx/int_types/intptr.hpp
+++ stx/int_types/intptr.hpp
@@ -2,6 +2,20 @@
 #define STX_INT_TYPES_INTPTR_HPP
 
 //  Typedefs for intptr_t & uintptr_t.
+//
+//  We are using template metaprogramming to make sure that we select
+//  integer of minimal size for (u)intptr_t. More simple version would be
+//  like this (it selects integer of maximum size, but avoids problems
+//  with padding bits in integers, which is really rare problem nowadays):
+//  
+//  #include <limits.h>
+//  #if defined(LLONG_MAX) || defined(LONG_LONG_MAX)
+//  typedef long long intptr_t;
+//  typedef unsigned long long uintptr_t;
+//  #else
+//  typedef long intptr_t;
+//  typedef unsigned long uintptr_t;
+//  #endif
 
 #include <stddef.h>
 #include <limits.h>
