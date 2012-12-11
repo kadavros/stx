c00d8c40b6535b2f0a45845b30235519b7c82523
kadavros <kadavros@gmail.com>
2012-12-11 19:20:41 +0400

    std_allocator

---
+++ stx/std_allocator.hpp
@@ -0,0 +1,105 @@
+#ifndef STX_STD_ALLOCATOR_HPP
+#define STX_STD_ALLOCATOR_HPP
+
+#include <new>
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+namespace stx {
+
+template <typename T>
+class std_allocator
+{
+public:
+    
+    typedef T value_type;
+    typedef T* pointer;
+    typedef const T* const_pointer;
+    typedef T& reference;
+    typedef const T& const_reference;
+    typedef size_t size_type;
+    typedef ptrdiff_t difference_type;
+    
+    template <typename T1>
+    struct rebind
+    {
+        typedef std_allocator<T1> other;
+    };
+    
+    std_allocator() throw()
+    {
+    }
+    
+    std_allocator(const std_allocator&) throw()
+    {
+    }
+    
+    template <typename T1>
+    std_allocator(const std_allocator<T1>&) throw()
+    {
+    }
+    
+    ~std_allocator() throw()
+    {
+    }
+    
+    pointer address(reference x) const
+    {
+        return &x;
+    }
+    
+    const_pointer address(const_reference x) const
+    {
+        return &x;
+    }
+    
+    pointer allocate(size_type n, const void* = 0)
+    {
+        if (n > this->max_size()) {
+            throw std::bad_alloc();
+        }
+        pointer ret = static_cast<T*>(::operator new(n * sizeof(T)));
+        if (!ret) {
+            throw std::bad_alloc();
+        }
+        return ret;
+    }
+    
+    void deallocate(pointer p, size_type)
+    {
+        assert(p);
+        ::operator delete(p);
+    }
+    
+    size_type max_size() const throw() 
+    {
+        return size_t(-1) / sizeof(T);
+    }
+    
+    void construct(pointer p, const T& val) 
+    {
+        ::new ((void*) p) value_type(val);
+    }
+    
+    void destroy(pointer p)
+    {
+        p->~T();
+    }
+};
+
+template <typename T>
+inline bool operator==(const std_allocator<T>&, const std_allocator<T>&)
+{
+    return true;
+}
+
+template <typename T>
+inline bool operator!=(const std_allocator<T>&, const std_allocator<T>&)
+{
+    return false;
+}
+
+} // namespace stx
+
+#endif // STX_STD_ALLOCATOR_HPP
